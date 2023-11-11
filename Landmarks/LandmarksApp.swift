c0d7a0c2a7c2fbc686e257488508937db81fdbdb
marufkhan09 <maruf1609@gmail.com>
2023-11-11 23:57:48 +0600

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Maruf Khan on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
